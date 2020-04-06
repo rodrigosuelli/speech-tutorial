58ac45b0e79e4def225c35f7b0d16e02da3d34c3
Rodrigo Suélli <rodrigosuellli@gmail.com>
2020-04-06 10:59:54 -0300

    Aplication finished

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
 const greetings = [
-    'Olá, tudo vai ficar bem',
-    'Bom dia',
-    'Boa noite'
+    'Im good you little piece of love',
+    'Doing good homeboi',
+    'leave me alone'
 ];
 
+const weather = ['Weather is fine', 'You need a tan'];
+
 try {
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechRecognition = 
+        window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
 
     recognition.onstart = () => {
@@ -21,24 +24,22 @@ try {
 
         const transcript = event.results[current][0].transcript;
         content.textContent = transcript;
-
         readOutLoud(transcript);
     };
-
 
     //add the Listener to the btn
     btn.addEventListener('click', () => {
         recognition.start();
     })
     
-
     function readOutLoud(message) {
         const speech = new SpeechSynthesisUtterance();
 
-        speech.text = 'A Google Cloud Text-to-Speech converte texto de mais de 30 idiomas e variações em fala e conta com mais de 180 vozes. Ela aplica a pesquisa inovadora sobre síntese de fala (WaveNet) e as redes neurais avançadas do Google para oferecer áudio de alta fidelidade. Com a API fácil de usar, é possível criar interações realistas com os usuários e transformar o atendimento ao cliente e a interação com dispositivos e outros aplicativos.';
+        speech.text = "i don't know what you said";
 
-        if (message.includes('Bom dia')) {
-            const finalText = greetings[Math.floor(Math.random() * greetings.length)];
+        if (message.includes("i don't know what you said")) {
+            const finalText = 
+                greetings[Math.floor(Math.random() * greetings.length)];
             speech.text = finalText;
         }
 
@@ -46,31 +47,9 @@ try {
         speech.rate = 1;
         speech.pitch = 1;
 
-       // speech.text = message;
         window.speechSynthesis.speak(speech);
-        console.log(message);
-        console.log(speech);
-
-
-        addEventListener('keypress', event => {
-            console.log(event);
-            if (event.code == 'Space') {
-    
-                console.log('Space bar pressed');
-    
-                // if (window.speechSynthesis.paused != true) {
-                //     console.warn('WTF')
-                //     window.speechSynthesis.pause();
-    
-                // }
-
-                // if (window.speechSynthesis.paused == true) {
-                //     window.speechSynthesis.resume();
-                // }
-
-            }
-        });
-
+        // console.log(message);
+        // console.log(speech);
     }
 
 } catch (err) {
